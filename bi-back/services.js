0871d95407b700e5ab53fece86622261716fb240
Vinicius Lidington <vlidington@gmail.com>
2025-02-12 23:19:51 -0300

    criacao de metodos get e post relocacoes

--- bi-back/services.js
+++ bi-back/services.js
@@ -7,25 +7,38 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/relocacoes", async (req, res) => {
+    
+    await prisma.RelocationProduct.create({
+        data: {
+            title: req.body.title,
+            category: req.body.category,
+            number: req.body.number,
+            description: req.body.description,
+        },
+    });
+    res.status(201).json(req.body);
+});
+
+
 app.get("/relocacoes", async (req, res) => {
 
-  let relocacoes = [];
+  let relocacoes = [];  
 
   if (req.query) {
-    relocacoes = await prisma.relocacao.findMany({
+    relocacoes = await prisma.RelocationProduct.findMany({
       where: {
-        data: {
-          title: req.query.title,
-          category: req.query.category,
-          number: req.query.number,
-          description: req.query.description,
-        },
-      },
-    });
+        title: req.query.title,
+        category: req.query.category
+      },  
+    });  
   } else {
-    relocacoes = await prisma.relocacao.findMany();
-  };
+    relocacoes = await prisma.RelocationProduct.findMany();  
+  };  
 
   res.status(200).json(relocacoes);
-  
-});
+
+});  
+
+
+app.listen(3000)
