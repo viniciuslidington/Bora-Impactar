caf5395000582b7d1726fcda6534e30cd56d8001
Vinicius Lidington <vlidington@gmail.com>
2025-03-10 22:04:36 -0300

    ajuste token retorno

--- bi-back/services/login.js
+++ bi-back/services/login.js
@@ -63,13 +63,13 @@ app.post("/login", async (req, res) => {
       const data = await response.json();
       const processedData = processData(data.ngo);
   
-      await prisma.ONGdata.upsert({
+      await prisma.oNGdata.upsert({
         where: { id: processedData.id },
         update: processedData,
         create: processedData,
       });
   
-      const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: "30s" });
+      const token = jwt.sign({ email, user: data.user.name, userData: data }, SECRET_KEY, { expiresIn: "1h" });
   
       res.cookie("token", token, {
         httpOnly: true,
@@ -84,20 +84,23 @@ app.post("/login", async (req, res) => {
     }
   });
 
-app.get("/auth", async (req, res) => {
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({ error: "Token não fornecido" });
-  }
-
-  jwt.verify(token, SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: "Token inválido" });
+  app.get("/auth", async (req, res) => {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ error: "Token não fornecido" });
     }
-    res.json({ message: "Rota protegida", user: decoded.username });
+  
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: "Token inválido" });
+      }
+  
+      console.log("Token decodificado:", decoded); // Verifique a estrutura
+  
+      // Retorna um objeto com `user`, garantindo que `name` esteja presente
+      res.json({user: decoded.user, userData: decoded.userData });
+    });
   });
-});
 
 app.post("/logout", (req, res) => {
     res.clearCookie('token');
