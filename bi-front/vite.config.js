f489a7276868df8e4fa36b2713972323b54193f5
Cadu Falcão <kddu422@gmail.com>
2025-03-17 17:05:29 -0300

    configurações implementadas

--- bi-front/vite.config.js
+++ bi-front/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "./",  // TENTE ISSO para corrigir o 404 no preview
+  plugins: [react(), tailwindcss()],
+  base: "./", // TENTE ISSO para corrigir o 404 no preview
 });
