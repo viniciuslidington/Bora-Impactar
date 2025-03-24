89502313f5a286890f5d0e74b284a55a9734b12c
Vinicius Lidington <vlidington@gmail.com>
2025-03-23 23:26:46 -0300

    search

--- bi-back/proxy.js
+++ bi-back/proxy.js
@@ -7,8 +7,9 @@ import dataRoutes from "./services/data.js";
 import loginRoutes from "./services/login.js";
 import logoutRoutes from "./services/logout.js";
 import repasseRoutes from "./services/repasse.js";
-import serchRoutes from "./services/serchrepasse.js";
+import serchRoutes from "./services/searchrepasse.js";
 import solicitacoesRoutes from "./services/solicitacoes.js";
+import searchSolicitacoesRoutes from "./services/searchsolicitacoes.js";
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use("/logout", logoutRoutes);
 app.use("/repasse", repasseRoutes);
 app.use("/search-repasse", serchRoutes);
 app.use("/solicitacao", solicitacoesRoutes);
+app.use('/search-solicitacao', searchSolicitacoesRoutes);
 
 // Definindo a porta 3000
 app.listen(3000, () => {
