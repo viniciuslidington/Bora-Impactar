6b3d424481e21b4631bff0af864e86b3ec2a9c87
Cadu Falcão <kddu422@gmail.com>
2025-03-31 19:42:01 -0300

    correções de imagens e paginação no pexels

--- bi-front/src/components/SearchPosts/SearchPostVol.jsx
+++ bi-front/src/components/SearchPosts/SearchPostVol.jsx
@@ -86,9 +86,11 @@ export default function SearchPostVol({ data = {}, isLoading = false }) {
     >
       <div className="relative flex w-full items-center gap-2">
         <img
-
-          src={ong_Imagem || placeholderImg}
-
+          src={
+            ong_Imagem === "undefined" || ong_Imagem === undefined
+              ? placeholderImg
+              : ong_Imagem
+          }
           alt=""
           className="h-14 w-14 rounded-[50%] border border-[#9c9c9c81] object-cover"
         />
@@ -126,9 +128,7 @@ export default function SearchPostVol({ data = {}, isLoading = false }) {
         </p>
       </div>
       <img
-
         src={post_Imagem || placeholderImg}
-
         alt=""
         className="ml-auto h-[164px] w-[164px] rounded-sm border border-[#9c9c9c81] object-cover"
       />
