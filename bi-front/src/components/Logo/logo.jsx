0b4612ad3031a3ce1a51eff53110f31670daf52b
Cadu Falcão <kddu422@gmail.com>
2025-04-08 11:38:36 -0300

    cursor pointer na logo

--- bi-front/src/components/Logo/logo.jsx
+++ bi-front/src/components/Logo/logo.jsx
@@ -9,7 +9,7 @@ export default function Logo({ className }) {
     <img
       src={logo}
       alt="Logo"
-      className={className}
+      className={`${className} cursor-pointer`}
       onClick={() => navigate("/")}
     />
   );
